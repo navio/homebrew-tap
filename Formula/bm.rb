d307b148ab8e53409eb0b7ece971829063339225
Alberto Navarro <phonograph@alberto.pub>
2026-04-10 19:58:16 -0400

    bm 0.3.3 revision 1

--- Formula/bm.rb
+++ Formula/bm.rb
@@ -1,9 +1,10 @@
 class Bm < Formula
   desc "CLI bookmarks manager"
   homepage "https://github.com/navio/bookmarks"
-  url "https://github.com/navio/bookmarks/archive/refs/tags/v0.3.3.tar.gz"
-  sha256 "0794bba7efa9b0cf00f9b082dc18fc5c8c47ea8492f1e242ddc557892cc83028"
+  url "https://github.com/navio/bookmarks/archive/9231b6ad1253877618a825fa646f8fe98446022a.tar.gz"
+  sha256 "43f83087294c90e0f1f0ae83029e3817b46f59192d1e8bee4b4cbd575dd51cdf"
   version "0.3.3"
+  revision 1
 
   depends_on "go" => :build
 
