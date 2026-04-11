46392246a68deb3d5e9716cf60cd222fe06fd1fa
Alberto Navarro <phonograph@alberto.pub>
2026-04-10 21:11:57 -0400

    bm 0.4.1

--- Formula/bm.rb
+++ Formula/bm.rb
@@ -1,9 +1,9 @@
 class Bm < Formula
   desc "CLI bookmarks manager"
   homepage "https://github.com/navio/bookmarks"
-  url "https://github.com/navio/bookmarks/archive/9231b6ad1253877618a825fa646f8fe98446022a.tar.gz"
-  sha256 "43f83087294c90e0f1f0ae83029e3817b46f59192d1e8bee4b4cbd575dd51cdf"
-  version "0.4.0"
+  url "https://github.com/navio/bookmarks/archive/fba19131a73d09d6a1a5c67e402a33e7687feb07.tar.gz"
+  sha256 "84eae8464ca868dfff43e02e28aad251149df2f22689f0948da867e4d3e880da"
+  version "0.4.1"
 
   depends_on "go" => :build
 
