1659490be9c54daa5c718d320b3bd946ff932c83
Alberto Navarro <phonograph@alberto.pub>
2026-04-10 20:02:42 -0400

    bm 0.4.0

--- Formula/bm.rb
+++ Formula/bm.rb
@@ -3,8 +3,7 @@ class Bm < Formula
   homepage "https://github.com/navio/bookmarks"
   url "https://github.com/navio/bookmarks/archive/9231b6ad1253877618a825fa646f8fe98446022a.tar.gz"
   sha256 "43f83087294c90e0f1f0ae83029e3817b46f59192d1e8bee4b4cbd575dd51cdf"
-  version "0.3.3"
-  revision 1
+  version "0.4.0"
 
   depends_on "go" => :build
 
